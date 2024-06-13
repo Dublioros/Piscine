c8c109476dd0a7dbb399c5fa500e06e07057dbf3
Jose Carlo Gutierrez Schweitzer <jose.g.schweitzer@gmail.com>
2024-06-13 22:21:01 +0200

    ADD

--- E03FINAL/11-first_word/first_word.c
+++ E03FINAL/11-first_word/first_word.c
@@ -1,14 +1,16 @@
 #include <unistd.h>
 
-int main( int char ac, char **av)
+int main(int ac, char **av)
 {
-    int = i:
+    int i;
 
     i = 0;
-    if ( ac == 2)
+    if (ac == 2)
     {
+        // Omitir los espacios iniciales y tabulaciones
         while (av[1][i] == ' ' || av[1][i] == '\t')
             i++;
+        // Escribir los caracteres hasta el primer espacio o tabulación
         while (av[1][i] != '\0' && av[1][i] != ' ' && av[1][i] != '\t')
         {
             write(1, &av[1][i], 1);
